ea43857057466ae7fd9e69ba060c33be0655982e
Samathy Barratt <samathy@sbarratt.co.uk>
2019-01-24 23:26:32 +0000

    Added example file

---
+++ source/example.d
@@ -0,0 +1,28 @@
+module example;
+
+import core.thread;
+
+import primordial.init;
+import primordial.sdl.display;
+import primordial.primitives.shapes;
+import primordial.defaults: default_screen;
+import primordial.colors: red;
+
+int main()
+{
+
+    primordial.init.primordial_init(true);
+
+    sdl_window window = new sdl_window("Example");
+
+    rectangle r = new rectangle(0,0,10,10, red, window.get_renderer());
+    
+    r.render();
+
+    window.update();
+
+    Thread.sleep( dur!("seconds")( 2 ));
+
+    return 0;
+
+}
